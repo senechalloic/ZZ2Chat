b70ea4e9a5b7eb0015c33235c0a658c80c2efad8
Loïc Sénéchal <Loc Snchal>
2016-10-19 12:46:28 +0200

    Fonction connexion et inscription

--- src/login.php
+++ src/login.php
@@ -1,5 +1,5 @@
 <?php
-    $username = $_POST['username'];
+	$username = $_POST['username'];
     $password = $_POST['password'];
     $filename = '../db/users.txt';
     
--- src/new_account.php
+++ src/new_account.php
@@ -1,11 +1,17 @@
 <?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $filename = '../db/users.txt';
-    $fp = fopen($filename, 'a+');
-    fwrite ($fp, $username . "," . $password . "\n");
-    $fclose ($fp);
-    echo ("account created");
-    header("Location: "login.html"); 
-    die();
+if(isset($_POST['submit']))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$text = $username . "," . $password . "\n";
+	$fp = fopen('../db/users.txt', 'a+');
+	fwrite ($fp, $text);
+	
+
+	echo 'saved';
+	$fclose ($fp);
+	
+	header('../../static/html/index.html');
+	exit;
+}
 ?>
